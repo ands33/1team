f95c80aa89e43743df84a34e59e1e6a7bf069d81
shcho0123 <jsh980123@gmail.com>
2024-06-13 18:20:19 +0900

    조성혁0613

---
+++ src/main/java/kca/cbt/exam/GetTestListController.java
@@ -0,0 +1,16 @@
+package kca.cbt.exam;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GetTestListController {
+
+	@RequestMapping("/getTestList.do")
+	public String getTestList(TestVO vo, TestDAO testDAO, Model model) {
+		model.addAttribute("testList", testDAO.getTestList(vo));
+		return "exam.jsp";
+	}
+
+}
--- src/main/java/kca/cbt/exam/TestServiceImpl.java
+++ src/main/java/kca/cbt/exam/TestServiceImpl.java
@@ -1,18 +1,20 @@
-package kca.cbt.exam.impl;
+package kca.cbt.exam;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kca.cbt.exam.TestService;
-import kca.cbt.exam.TestVO;
+import kca.cbt.qna.QnaDAO;
 
 @Service("testSerivce")
 public class TestServiceImpl implements TestService {
 	@Autowired
 	private TestDAO testDAO;
 
+    public void setTestDAO(TestDAO testDAO) {
+        this.testDAO = testDAO;
+    }
 
 	public List<TestVO> getTestList(TestVO vo) {
 		// TODO Auto-generated method stub
