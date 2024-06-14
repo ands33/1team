333db5d375160f64b07627a7014aa92f628a57dd
DongRyeolYoon <drayoon9591@gamil.com>
2024-06-14 19:26:56 +0900

    6_14yoon3

--- src/main/java/kca/cbt/notice/UpdateNoticeController.java
+++ src/main/java/kca/cbt/notice/UpdateNoticeController.java
@@ -1,15 +1,14 @@
-package kca.cbt.notice;
-
-import ...;
-import .....;
-
-@Controller
-public class UpdateNoticeController {
-	@RequestMapping("/updateUser.do")
-	public String updateUser(UserVO vo, UserDAO userDAO) {
-		
-		userDAO.updateUser(vo);
-		return "getUserList.do";
-	}
-
-}
+/*
+ * package kca.cbt.notice;
+ * 
+ * import ...; import .....;
+ * 
+ * @Controller public class UpdateNoticeController {
+ * 
+ * @RequestMapping("/updateUser.do") public String updateUser(UserVO vo, UserDAO
+ * userDAO) {
+ * 
+ * userDAO.updateUser(vo); return "getUserList.do"; }
+ * 
+ * }
+ */
