c1dd5a2daad1ccc9ed111b3643e1221c7f3fb5be
ands33 <zip0317@naver.com>
2024-06-14 12:26:45 +0900

    list구현기능추가

--- src/main/java/kca/cbt/qna/getQnaListController.java
+++ src/main/java/kca/cbt/qna/getQnaListController.java
@@ -9,6 +9,21 @@
 //	@RequestMapping(value="/getQnaList.do")
 //	public String getQnaList(QnaVO vo, QnaDAO qnaDAO, Model model) {
 //		model.addAttribute("qnaList", qnaDAO.getQnaList(vo));
-//		return "QnaList.jsp";
+//		return "qna/qna.jsp";
 //	}
 //}
+package kca.cbt.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class getQnaListController {
+
+    @RequestMapping(value = "/getQnaList.do")
+    public String getQnaList(QnaVO vo, QnaDAO qnaDAO, Model model) {
+        model.addAttribute("qnaList", qnaDAO.getQnaList(vo));
+        return "qna/qna.jsp";
+    }
+}
