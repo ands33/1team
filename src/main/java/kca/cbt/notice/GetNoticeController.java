248f828d1e05e0960e0979eaa07e17af85a2ca5f
DongRyeolYoon <drayoon9591@gamil.com>
2024-06-15 15:29:35 +0900

    6_15yoon

--- src/main/java/kca/cbt/notice/GetNoticeController.java
+++ src/main/java/kca/cbt/notice/GetNoticeController.java
@@ -1,21 +1,21 @@
-//package kca.cbt.notice;
-//
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import kca.cbt.notice.NoticeVO;
-//import kca.cbt.notice.NoticeDAO;
-//
-//@Controller
-//public class GetNoticeController {
-//
-//	@RequestMapping("/getNotice.do")
-//	public ModelAndView getNoticeList(NoticeVO vo, NoticeDAO boardDAO, ModelAndView mav) {
-//		mav.addObject("notice",NoticeDAO.getnotice(vo)); // Model 정보 저장
-//		mav.setViewName("getNotice.jsp"); // View 정보 저장
-//		return mav;
-//	}
-//
-//}
+package kca.cbt.notice;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kca.cbt.notice.NoticeVO;
+import kca.cbt.notice.NoticeDAO;
+
+@Controller
+public class GetNoticeController {
+
+	@RequestMapping("/getNotice.do")
+	public ModelAndView getNoticeList(NoticeVO vo, NoticeDAO noticeDAO, ModelAndView mav) {
+		mav.addObject("notice",NoticeDAO.getnotice(vo)); // Model 정보 저장
+		mav.setViewName("getNotice.jsp"); // View 정보 저장
+		return mav;
+	}
+
+}
