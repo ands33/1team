a4b9d59a4331d5c7befc7cba2f72c92e54f27a1f
ands33 <zip0317@naver.com>
2024-06-20 18:36:08 +0900

    일단 해결 6/20

--- src/main/java/kca/cbt/view/exam_subjects/Exam_subjectUpdateController.java
+++ src/main/java/kca/cbt/view/exam_subjects/Exam_subjectUpdateController.java
@@ -3,19 +3,18 @@ package kca.cbt.view.exam_subjects;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import kca.cbt.exam_subjects.Exam_subjectService;
+import kca.cbt.exam_subjects.Exam_subjectDAO;
 import kca.cbt.exam_subjects.Exam_subjectVO;
 
 @Controller
 public class Exam_subjectUpdateController {
 
     @RequestMapping(value = "UpdateE.do")
-    public String updateE(@RequestParam Map<String, String> paramMap, Exam_subjectVO exam_subjectVO) {
+    public String updateE(@RequestParam Map<String, String> paramMap, Exam_subjectDAO exam_subjectDAO) {
         Map<String, Exam_subjectVO> es = new HashMap<>();
 
         for (String key : paramMap.keySet()) {
@@ -39,7 +38,7 @@ public class Exam_subjectUpdateController {
             }
         }
 
-        exam_subjectService.updateE(es);
+        exam_subjectDAO.updateE(es);
         return "redirect:getEList.do"; // 업데이트 후 리스트 페이지로 리다이렉트
     }
 }
